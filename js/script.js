e01319763cb92655f19d57a4d25df21f6838f699
sergei <ravinski.sergei@gmail.com>
2022-10-18 13:16:53 +0200

    Changed styles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
-let slideIndex = 0;
-showSlides();
+let index = 1;
+showSlides(index);
 
-function showSlides () {
-	let i;
-	let slides = document.getElementsByClassName('slideshow__image');
-	let dots = document.querySelectorAll('dot');
+//const plusSlides = document.querySelector('.button__next');
+//const minusSlides = document.querySelector('.button__previous');
 
-	for (i = 0); i< dots.length; i+
+
+// Next/previous controls
+
+
+//function minusSlides(n) {
+//	showSlides(index -= n);
+//}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(index = n);
 }
+
+function plusSlides(n) {
+	showSlides(index += n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.querySelectorAll('.slideshow__card');
+	let dots = document.querySelectorAll('.dot');
+	
+	if (n > slides.length) {
+		index = 1;
+	} 
+
+	if (n < 1) {
+		index = slides.length;
+	}
+	
+	for (i = 0; i < slides.length; i +=1) {
+		slides[i].style.display = "none"; 
+	}
+	
+	for (i = 0; i < dots.length; i +=1) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[index-1].style.display = " block"; 
+	dots[index-1].className += " active";
+}
+
 
 
 
