a8bdd5abd3e1c0c9b1d371232ad7046bbd8b4061
Anna E <112336610+AnnaEikrem@users.noreply.github.com>
2022-10-18 12:01:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides () {
+	let i;
+	let slides = document.getElementsByClassName('slideshow__image');
+	let dots = document.querySelectorAll('dot');
+
+	for (i = 0); i< dots.length; i+
+}
+
+
+
+
+
+// const dots = document.querySelectorAll('.dot');
+// const slideImages = document.querySelectorAll('.slideshow__image');
+
+
+// dots.addEventlistener('click', handleDotClick);
+
+// function handleDotClick(event){
+// 	if()
+// }
+
+
+
+
+
+
+
+
+
