749574a33f72b9284102b8b893e1c2ef10bbb8ab
sergei <ravinski.sergei@gmail.com>
2022-10-20 17:25:40 +0200

    Added control with arrows on the keyboard

--- js/script.js
+++ js/script.js
@@ -1,46 +1,27 @@
 let index = 1;
 showSlide(index);
 
-//let dots = document.querySelectorAll('.dot');
-
-//for (let index = 1; index < dots.length; index++) {
-//	let dot = dots[index];
-//	dot.addEventListener('click', changeSlides(index));
-//}
-
-
-
-//function slidesCollectionAuto() {
-//	let slidesCollectionAuto = document.querySelectorAll('.slideshow__card');
-//	changeSlideAuto(+1);
-//	setTimeout(slidesCollectionAuto, 1000);
-//}
-
-
-
-
-//change querySelector to getElementById and then 
 const buttonPrevious = document.querySelector('#previousButton');
 const buttonNext = document.querySelector('#nextButton');
 
-buttonPrevious.addEventListener('click', handlebuttonPreviousOnClick);
-buttonNext.addEventListener('click', handlebuttonNextOnClick);
+buttonPrevious.addEventListener('click', handleButtonPreviousOnClick);
+buttonNext.addEventListener('click', handleButtonNextOnClick);
 
-function handlebuttonPreviousOnClick() {
-	changeSlides(-1)
+function handleButtonPreviousOnClick() {
+	changeSlides(-1);
 }
 
-function handlebuttonNextOnClick() {
-	changeSlides(1)
+function handleButtonNextOnClick() {
+	changeSlides(1);
 }
 
 function changeSlides(direction) {
 	showSlide(index += direction);
 }
 
+
 //reset all to inactive and show new active
 function showSlide(newIndex) {
-	let i;
 	let slidesCollection = document.querySelectorAll('.slideshow__card');
 	
 	let dots = document.querySelectorAll('.dot');	
@@ -56,7 +37,7 @@ function showSlide(newIndex) {
 	}
 	
 	//hide all slides
-	for (let index = 0; index < slidesCollection.length; index++) {
+	for (let index = 0; index < slidesCollection.length; index += 1) {
 		slidesCollection[index].style.display = "none"; 
 	}
 
@@ -69,88 +50,29 @@ function showSlide(newIndex) {
 
 	//show dot with active index
 	dots[index -1].className += " active";	
+
+	//change picture auto
+	//setTimeout('changeSlides(1)', 3000);
 }
 
+//change picture with arrows on keyboard
+const dots = document.querySelectorAll('.dot');
 
-//function changeSlides(direction) {
-//	showSlide(index += direction);
-//	if (i < slidesCollection.length -1) {
-//		i ++;
-//	} else {
-//		i = 0;
-//	}
-//}
-//setTimeout('changeSlides()', 1000);
+for (let index = 0; index < dots.length; index += 1) {
+	dots[index].addEventListener('click', handleDotsClick);
+}
 
+window.addEventListener('keydown', handleWindowKeydown);
 
+function handleDotsClick(event) {
+	goToSpecificSlide(event);
+}
 
+function handleWindowKeydown(event) {
+	if (event.key === 'ArrowLeft') {
+		changeSlides(-1);
 
-
-//const i = 0;
-
-//let images = [];
-
-//let image = document.querySelectorAll('.slideshow__card');
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-//function slideInterval = 
-//let slideInterval = newIndex(slidesCollection, 1000);
-
-//console.log(slideInterval);
-
-
-
-//function dotImages () {
-//	const dotImage = document.querySelectorAll('')
-//}
-
-
-
-
-
-
-//dots.document.addEventListener('click', currentDot)
-
-//function currentDot
-
-
-//for (i = 0; i < dots.length; i +=1) {									
-//	dots[i].className = dots[i].className.replace(" active", ""); 
-//}
-//slides[index-1].style.display = "block"; 
-//dots[index-1].className += ".active";
-
-
-//let dotsWrapper = document.querySelectorAll('#dotsWrapper');
-//let dots = dotsWrapper.querySelectorAll('.dot');
-
-//function currentDot() {
-//	togglePicture()
-//}
-
-
-
-
-
-
-
-
-
+	} else if (event.key === 'ArrowRight') {
+		changeSlides(1);
+	}
+}
