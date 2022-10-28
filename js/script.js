34e4b29ddf0e983f19177dd52f9be42bb2ce1733
sergei <ravinski.sergei@gmail.com>
2022-10-28 12:56:48 +0200

    Added functions

--- js/script.js
+++ js/script.js
@@ -1,67 +1,51 @@
-let index = 1;
-showSlide(index);
-
+//Variables
+let currentSlideIndex = 0;
+const slideshowCards = document.querySelectorAll('.slideshow__card');
 const buttonPrevious = document.querySelector('#previousButton');
 const buttonNext = document.querySelector('#nextButton');
+const dots = document.querySelectorAll('.dot')
 
-buttonPrevious.addEventListener('click', handleButtonPreviousOnClick);
-buttonNext.addEventListener('click', handleButtonNextOnClick);
+//Event listeners
+buttonPrevious.addEventListener('click', handleButtonPreviousClick);
+buttonNext.addEventListener('click', handleButtonNextClick);
+//dots.addEventListener('keyup', handleButtonDotClick);
 
-function handleButtonPreviousOnClick() {
-	changeSlides(-1);
+//Event handlers
+function handleButtonPreviousClick(event) {
+	decreaseCurrentSlideIndex();
+	updateSlideHtml();
 }
 
-function handleButtonNextOnClick() {
-	changeSlides(1);
+function handleButtonNextClick(event) {
+	increaseCurrentSlideIndex();
+	updateSlideHtml();
 }
 
-function changeSlides(direction) {
-	showSlide(index += direction);
-}
+//Methods
 
-//reset all to inactive and show new active
-function showSlide(newIndex) {
-	let slidesCollection = document.querySelectorAll('.slideshow__card');
-	
-	let dots = document.querySelectorAll('.dot');	
-
-	//if last picture - change to first
-	if (newIndex > slidesCollection.length) {
-		index = 1;
-	} 
-
-	//if first picture - change to last
-	if (newIndex < 1) {
-		index = slidesCollection.length;
+function decreaseCurrentSlideIndex() {
+	if (currentSlideIndex > 0) {
+		currentSlideIndex -= 1;
+	} else {
+		currentSlideIndex = slideshowCards.length;
 	}
-	
-	//hide all slides
-	for (let index = 0; index < slidesCollection.length; index += 1) {
-		slidesCollection[index].style.display = "none"; 
-	}
-
-	//unselect all dots
-	for (let index = 0; index < dots.length; index += 1) {
-		dots[index].className = dots[index].className.replace(' active', ''); 	
-	}
-	//show slide with active index
-	slidesCollection[index-1].style.display = 'block'; 
-
-	//show dot with active index
-	dots[index -1].className += ' active';	
-
-	//change picture auto
-	//setTimeout('changeSlides(1)', 3000);
+	//console.log(`go to slide index ${currentSlideIndex}`);
 }
 
-//change picture with arrows on the keyboard
-window.addEventListener('keydown', handleWindowKeydown);
-
-function handleWindowKeydown(event) {
-	if (event.key === 'ArrowLeft') {
-		changeSlides(-1);
-
-	} else if (event.key === 'ArrowRight') {
-		changeSlides(1);
+function increaseCurrentSlideIndex() {
+	if (currentSlideIndex < slideshowCards.length) {
+		currentSlideIndex += 1;
+	} else {
+		currentSlideIndex = 0;
 	}
+	//console.log(`go to slide index ${currentSlideIndex}`);
 }
+
+function updateSlideHtml() {
+	slideshowCards[currentSlideIndex].classList.add('slideshow__card-visible');
+
+}
+
+
+
+
